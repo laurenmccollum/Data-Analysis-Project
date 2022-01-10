b2a0b263637e527b581ca4ecd57e481601d9cb2e
laurenmccollum <92272703+laurenmccollum@users.noreply.github.com>
2022-01-10 17:04:49 +0000

    Update part2_q1.py

--- part2_q1.py
+++ part2_q1.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import astropy.io.ascii as ascii
 from GLS import GLS as GLS
+from scipy import optimize
 
 #Data for RV of system %i circuit
 #t time in days (JD-2,400,000)
@@ -22,67 +23,86 @@ RVerror = np.array(data['e_RV'])
 
 # f = np.fft.fftfreq(t.shape[0])
 
-#you also want to oversample the periods to get a nice smooth periodogram
-#mine is not smooth so whats up
-P = np.logspace(-1.3,np.log10(30./2.),int(1e4)) 
+P = np.logspace(-1.3,np.log10(30./2.),int(1e5))
 
 p_om,a_om,b_om=GLS(t,RV,RVerror,P) 
 
-plt.plot(p_om)
-plt.show()
+# plt.plot(P,p_om)
+# plt.show()
 
 #question 2
+index = np.argmax(p_om)
+strongest = P[index]
+# print(strongest)
 
-
-
-
-
-
-
-
-
-
-#things needed to plot periodogram
-#t,mdl_noise,sig,P
-
-#mdl noise = mdl+np.random.normal(0.,5.,t.shape[0])
-#mdl = np.sin(2.*np.pi*t/Per+np.pi/4) 
-#need period, K and phase offset
-#K = amplitude
-
-#sig = t*0.+5.
-#dont really understand multiplying time by 0
-
-#period. no idea how to find
-
-# t0 = t[1] #or could i just use t0 = 0???
-# P = #period of orbit
-# K = #radial velocity semi amplitude
-# phase = #phase offset, a number between 0 and 1
-# v = K * np.sin(2*np.pi*(t/P-phase))
-
-#questions:
-#how to find the period
-#how to find radial velocity semi amplitude
-#do I just choose a phase offset
-
-# t=np.arange(0,30.,0.01) 
-# Per=4.5 
-# mdl=np.sin(2.*np.pi*t/Per+np.pi/4) 
-# mdl_noise1=mdl+np.random.normal(0.,0.1,t.shape[0]) 
-# mdl_noise2=mdl+np.random.normal(0.,5.,t.shape[0]) 
-# sig1=t*0.+0.1 
-# sig2=t*0.+5. 
-# plt.plot(t,mdl_noise2,'k.') #this is the noise i think
-# plt.plot(t,mdl,linewidth=3) 
+#question 3
+phi = (t/strongest) %1
+# plt.plot(phi,RV,'o')
 # plt.show()
 
-# plt.plot(t,mdl_noise1,'k.') #this is the noise i think
-# plt.plot(t,mdl,linewidth=3) 
-# plt.show()
+steps = 50
 
-# P=np.logspace(-1.3,np.log10(30./2.),int(1e5)) 
-# p_om,a_om,b_om=GLS(t,mdl_noise2,sig2,P) 
+#get K and D values from looking at the graph
+K = 55
+P = strongest
+D = 3.8
 
-# plt.plot(p_om) #is this just how you're meant to plot it?
-# plt.show()
+intervalK = 0.2
+intervalP = 4.05e-6
+intervalD = 0.015
+
+gridK = np.arange(K-intervalK*steps,K+(intervalK)*(steps-1.5),intervalK)
+gridP = np.arange(P-intervalP*steps,P+(intervalP)*(steps-1.5),intervalP)
+gridD = np.arange(D-intervalD*steps,D+(intervalD)*(steps-1.5),intervalD)
+
+#calculate chi squared for each point on the grid (you will need a nested loop)
+
+chi = []
+
+def radialvelocity(K,t,P,D):
+  return K*np.sin(2*np.pi*((t/P)-D))
+
+for i in range(99):
+  chi.append([])
+  for j in range(99):
+     chi[i].append([])
+     for k in range(99):
+      chi[i][j].append(np.sum((((RV-radialvelocity(gridK[i],t,gridP[j],gridD[k],))**2))/RVerror**2))
+
+chi = np.array(chi)
+
+minchi = np.amin(chi)
+
+index = np.unravel_index(np.argmin(chi,axis=None),chi.shape)
+
+plt.plot(gridK,chi[:,index[1],index[2]])
+plt.axhline(y=(minchi+3.53), color='r', linestyle='-')
+sigma = np.argwhere(np.diff(np.sign((np.min(chi[i:,index[1],index[2]]) + 3.53) - chi[:,index[1],index[2]]))).flatten()
+print('K', gridK[index[0]], '+/-',np.abs(gridK[index[0]]-(((gridK[sigma[0]])))))
+plt.show()
+
+print(sigma)
+
+plt.plot(gridP,chi[index[0],:,index[2]])
+plt.axhline(y=(minchi+3.53), color='r', linestyle='-')
+sigma = np.argwhere(np.diff(np.sign((np.min(chi[index[0], :, index[2]]) + 3.53) - chi[index[0], :, index[2]]))).flatten()
+print('P', gridP[index[1]], '+/-', np.abs(gridP[index[1]]-((gridP[sigma[0]]+gridP[sigma[1]])/2)))
+plt.show()
+
+print(sigma)
+
+plt.plot(gridD,chi[index[0],index[1],:])
+plt.axhline(y=(minchi+3.53), color='r', linestyle='-')
+sigma = np.argwhere(np.diff(np.sign((np.min(chi[index[0],index[1],:]) + 3.53) - chi[index[0],index[1],:]))).flatten()
+print('delta phi', gridD[index[2]], '+/-', np.abs(gridD[index[2]]-((gridD[sigma[0]]+gridD[sigma[1]])/2)))
+plt.show()
+
+print(sigma)
+
+#for 3 its 3.53
+#for 4 its 4.72
+
+#question 4
+
+popt,pcov = scipy.optimize.curve_fit(radioactivedecay,t,A1,sigma=A1error)
+print('A1',popt)
